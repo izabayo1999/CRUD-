dffd7f811e6cd1a81ac5a5a7d161deab09686b76
izabayo1999 <valentineizby@gmail.com>
2022-01-27 11:59:10 +0200

    commited

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
